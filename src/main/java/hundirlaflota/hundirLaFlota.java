b1c3cfe549bd7534e0975e796aafb56a903293a1
KIKECASA <enrmarcre@alu.edu.gva.es>
2022-01-06 11:37:12 +0100

    Primer código

---
+++ src/main/java/hundirlaflota/hundirLaFlota.java
@@ -0,0 +1,31 @@
+
+package hundirlaflota;
+import java.util.*;
+
+
+
+public class hundirLaFlota {
+  
+    
+    
+    public static char tablero [][];//creamos una función que llamaremos tablero
+    
+    //creo dos funciones para marcar la longitud del eje X y del eje Y
+    public static int lineaX=10;
+    public static int lineaY=10; 
+    
+        
+    public static void dibujaTablero() {
+        
+        System.out.print (" 0 1 2 3 4 5 6 7 8 9 \n");
+        
+    }
+  
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        dibujaTablero();
+    }
+  } 
